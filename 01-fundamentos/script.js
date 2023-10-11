a107419b9be057934b106b9c902d9036aba3cc85
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-10-11 16:25:42 -0400

    Iniciando con JS nos quedamos para continur el 12

---
+++ 01-fundamentos/script.js
@@ -0,0 +1,28 @@
+// console.log(console.log('Hola Mundo')); // Nos da mensajes en la consola
+// En cualquier consola hacer el comando node, que nos permite ejecutar codigo JS sin necesidad del navegador
+
+// Variables
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a + b;
+// otra forma
+// let a = 10, b = 10, c = 10, d = 10, x = a + b; // En usa sola linea
+// Acomodando
+let a = 10,
+    b = 10,
+    c = 10,
+    d = 10,
+    x = a + b; // de esta forma igual se puede 
+
+
+console.log(x);   //clg
+// var b = 10;
+// const c = 10;
+// c = 30 // se inicializa la variable pero es mala pratica no declarar con let o var
+
+// c = 20; // No se puede hacer esto
+
+// a = 20; // Funciona
+// b = 30; // Funciona
